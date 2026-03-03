74c9e1449f58fb680b5daac9d18aa2403756a3e4
Pedro Corgnati <corgnati.pedro@gmail.com>
2026-03-03 01:48:03 -0300

    Allow inline scripts in CSP for production

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const securityHeaders = [
       "default-src 'self'",
       isDev
         ? "script-src 'self' 'unsafe-eval' 'unsafe-inline'"
-        : "script-src 'self'",
+        : "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' blob: data: https:",
       "font-src 'self' https://fonts.gstatic.com",
